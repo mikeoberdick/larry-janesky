a3e0bf26dff1a54a14e07bad155b2b9c32582142
Mike Oberdick <mike@designs4theweb.com>
2018-04-03 05:56:19 -0400

    Added Archive & Pagination Styling

--- archive.php
+++ archive.php
@@ -1,26 +1,21 @@
 <?php
 /**
- * The main template file.
+ * The Archive template file.
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
  * @package understrap
  */
 
 get_header(); ?>
 
-<div class="wrapper" id="index-wrapper">
+<div class="wrapper" id="archive-wrapper">
   <div id="content" tabindex="-1">
     <div id = "thinkDaily">
       <div id = "thinkDailyHeader" class="container-fluid" style = "background:url('<?php echo get_stylesheet_directory_uri(); ?>/img/think_daily_bg.jpg');">
         <div class = "container">
-        	<div class="row">
-        		<div class="col-sm-12 text-center">
-        			<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/think_daily_logo.png" alt="Think Daily by Larry Janesky">
+          <div class="row">
+            <div class="col-sm-12 text-center">
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/think_daily_logo.png" alt="Think Daily by Larry Janesky">
 
           <!-- ARCHIVE DROP DOWNS -->
               <div id = "archiveDropdowns" class = ".col-md-3 .offset-md-3">
@@ -86,14 +81,15 @@ get_header(); ?>
     </article><!-- #post-## -->
 
 <?php
-$postCount++; endwhile; endif;
-?>
+ $postCount++;
+  endwhile;
+  endif; ?>
+
   </div><!-- #postContainer -->
-  <div id="postNavContainer"> 
-        <?php understrap_pagination(); ?>
-  </div>
 </main>
 
+<!-- The pagination component -->
+<?php understrap_pagination(); ?>
 
 <div id = "thinkDailyForm" class="container">
   <img src= "<?php echo get_stylesheet_directory_uri(); ?>/img/think_daily_logo.png" alt="Think Daily by Larry Janesky">
